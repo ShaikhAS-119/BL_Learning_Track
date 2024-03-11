a1037160aacf23484c36dece53bb9771d0bab064
Shaikh Abid <alishaikh4199@gmail.com>
2024-03-11 16:10:00 +0530

    [Abid]Added: Completed with HashSet and SortedSet

---
+++ HashSets.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Structure
+{
+    internal class HashSets
+    {
+        //create and read
+        public static void HashDemo() {
+            Console.WriteLine("Element of Hash 1");
+            var hashvalue = new HashSet<int>();
+            hashvalue.Add(98);
+            hashvalue.Add(1);
+            hashvalue.Add(2);
+            hashvalue.Add(33);
+            hashvalue.Add(4);
+            foreach (var value in hashvalue)
+            {
+                Console.Write(" "+value);
+            }
+
+            Console.WriteLine("Element of Hash 2");
+            var hashvalue2 = new HashSet<int>() {32,65,87,45,};
+            foreach (var value in hashvalue2)
+            {
+                Console.WriteLine($"{value}");
+            }
+
+            //remove
+            Console.WriteLine("Count is " + hashvalue.Count + "before");
+            hashvalue.Remove(4);
+            foreach (var value in hashvalue)
+            {
+                Console.WriteLine($"{value}");
+            }
+            Console.WriteLine("Count is " + hashvalue.Count + "after");
+
+            //union of 2 set
+            Console.WriteLine("after merge: ");
+             hashvalue.UnionWith(hashvalue2);
+            foreach(var value in hashvalue)
+            {
+                Console.Write(" "+value);
+            }
+
+        }
+    }
+}
---
+++ SortedSets.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Structure
+{
+    internal class SortedSets
+    {
+        public static void SortedSetDemo()
+        {
+            //create and read
+            Console.WriteLine("First SortedSet");
+            SortedSet<int> values = new SortedSet<int> ();
+            values.Add(8);
+            values.Add(6);
+            values.Add(2);
+            values.Add(9);
+            foreach (int i in values)
+            {
+                Console.Write(" "+i);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("second SortedSet");
+            SortedSet<string> values2 = new SortedSet<string>() { "Today", "We", "Practiced", "Collections" };
+            foreach (string val in values2)
+            {
+                Console.WriteLine(val);
+            }
+
+            Console.WriteLine("max num is "+values.Max);
+            Console.WriteLine("Min is "+ values2.Min);
+
+            Console.WriteLine("Count of first set: " + values.Count);
+            values.Remove(6);
+            Console.WriteLine("Count of first set: " + values.Count);
+
+            Console.WriteLine("Count of sec set: " + values2.Count);
+            values2.Remove("We");
+            Console.WriteLine("Count of sec set: "+ values2.Count);
+
+        }
+    }
+}
