ea9bce4c0bf353983e99f15ed0c65b922408882b
Shaikh Abid <alishaikh4199@gmail.com>
2024-03-13 19:38:52 +0530

    [Abid]Added: Operation performed

---
+++ SortedDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Data_Structure
+{
+    internal class SortedDictionary
+    {
+        public static void SDictionaryDemo()
+        {
+          
+            SortedDictionary<int, string> sdic = new SortedDictionary<int, string>();
+            sdic.Add(23, "Sorted");
+            sdic.Add(19, "Thane");
+            sdic.Add(29, null);
+            sdic.Add(14, "Mumbai");
+
+            //check presence of key
+             Console.WriteLine(sdic.ContainsKey(29));
+
+            //Remove particular elemnent
+            sdic.Remove(29);
+
+            //clear all element
+            sdic.Clear();
+
+            //perform operation check given value is present or not. if present print 
+            //its key and value
+            string name = "Thane";
+            foreach (KeyValuePair<int, string> value in sdic)
+            {
+                if (value.Value == name)
+                {
+                    Console.WriteLine("key for the Thane is: "+value.Key + "and value is :"+ value.Value);
+
+                }
+                Console.WriteLine(value.Key + " and " + value.Value);
+            }
+
+        }
+    }
+}
---
+++ SortedList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Structure
+{
+    internal class SortedList
+    {
+        public static void SListsDemo()
+        {
+            SortedList<object,object> sortedlist = new SortedList<object,object>();
+            sortedlist.Add(3,"hello");
+            sortedlist.Add(4, null);
+            sortedlist.Add(5, 45);
+            sortedlist.Add(6, 98);
+
+            //foreach(object item in sortedlist) 
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //for(int i = 0; i <= sortedlist.Count; i++)
+            //{
+            //    Console.WriteLine(sortedlist[i]);
+            //}
+        }
+    }
+}
